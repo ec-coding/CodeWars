0d20aefee2657cafffaded2f5955ccc8d1e0c0c2
Edwin Choi <edwin.choi.wdev@gmail.com>
2023-01-13 17:35:52 -0600

    completed 8th kyu challenge

---
+++ 8th Kyu/How many lightsabers do you own.js
@@ -0,0 +1,30 @@
+// Inspired by the development team at Vooza, write the function that
+
+// accepts the name of a programmer, and
+// returns the number of lightsabers owned by that person.
+// The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+// Note: your function should have a default parameter.
+
+For example(Input --> Output):
+
+function howManyLightsabersDoYouOwn(name) {
+    if (name === "Zach") {
+      return 18
+    } else {
+      return 0
+    }
+  }
+  
+  // PREP
+  // PARAMETERS
+  // Enter in a name
+  // RETURNS
+  // If the name equals "Zach", return 18
+  // Otherwise, return 0
+  // EXAMPLES
+  // "Zach" --> 18
+  // "Blaine" --> 0
+  // PSEUDO
+  // We will use an if statement to determine whether to return 18 or 0, based on the name parameter
+  
